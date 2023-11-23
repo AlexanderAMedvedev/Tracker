0cdbbecdd1b8f0a98584dd2a36a9f8c2cd34dcfc
Александр Медведев <aleksandr@192.168.1.7>
2023-11-23 13:03:37 +0300

    DONE: created project, set iOS and iPhone orientation

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Александр Медведев on 23.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
