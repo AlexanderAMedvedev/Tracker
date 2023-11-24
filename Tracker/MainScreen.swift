962b1d14c9bfa6c817e0aa6079485108d0d59dcd
Александр Медведев <aleksandr@192.168.1.7>
2023-11-24 17:47:33 +0300

    DONE: made LaunchScreen (Storyboard), MainScreen (code).

---
+++ Tracker/MainScreen.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Александр Медведев on 23.11.2023.
+//
+
+import UIKit
+
+class MainScreen: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let trackers = Trackers()
+        trackers.tabBarItem = UITabBarItem(
+            title: "Трекеры",
+            image: UIImage(named: "trackerForTabBar"),
+            selectedImage: nil
+        )
+        let navCon = UINavigationController(rootViewController: trackers)
+        
+        
+        let statistics = Statistics()
+        statistics.tabBarItem = UITabBarItem(
+            title: "Статистика",
+            image: UIImage(named: "hare"),
+            selectedImage: nil
+        )
+        
+        self.setViewControllers([navCon, statistics], animated: false)
+    }
+
+    private func addLogoView() {
+        let logoView = UIImageView(image: UIImage(named: "Logo"))
+        logoView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logoView)
+        NSLayoutConstraint.activate([
+            logoView.heightAnchor.constraint(equalToConstant: 77.68),
+            logoView.widthAnchor.constraint(equalToConstant: 75),
+            logoView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+
+}
+
---
+++ Tracker/UIColor+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Extensions.swift
+//  Tracker
+//
+//  Created by Александр Медведев on 23.11.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var ypBlue: UIColor { UIColor(named: "Blue") ?? UIColor.blue }
+    static var ypWhiteDay: UIColor { UIColor(named: "WhiteDay") ?? UIColor.white }
+    static var ypBlackDay: UIColor { UIColor(named: "BlackDay") ?? UIColor.black }
+    
+}
